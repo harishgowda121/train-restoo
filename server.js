fbed21a2904267f6898947bc1357b877d1f31edd
harishgowda121 <161109259+harishgowda121@users.noreply.github.com>
2025-09-16 13:26:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const trainRoutes = require('./routes/trainRoutes');
+const hotelRoutes = require('./routes/hotelRoutes');
 
 const bodyParser = require('body-parser');
 
@@ -13,6 +14,8 @@ connectDB();
 app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/train', trainRoutes);
+app.use('/api/hotels', hotelRoutes);
+
 
 
 const PORT = process.env.PORT || 5000;
