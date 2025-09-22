ea94991d4e08751a393c465dc0dcc4ea7b7e3a72
harishgowda121 <161109259+harishgowda121@users.noreply.github.com>
2025-09-22 18:56:30 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { requestOtp, login, verifyOtp, verifyLoginOtp } = require('../controllers/authController');
+const {  loginOrSignup, verifyLoginOtp } = require('../controllers/authController');
 
 const router = express.Router();
 
-router.post('/signup', requestOtp);           // Request OTP for signup
-router.post('/verify-otp', verifyOtp);        // Verify OTP for signup
-router.post('/login', login);                 // Login with password → sends OTP
-router.post('/verify-login-otp', verifyLoginOtp);  // Verify OTP during login
+// router.post('/signup', requestOtp);           // Request OTP for signup
+// router.post('/verify-otp', verifyOtp);        // Verify OTP for signup
+router.post('/login', loginOrSignup);                 // Login with password → sends OTP
+router.post('/verify-otp', verifyLoginOtp);  // Verify OTP during login
 
 module.exports = router;
