31233faf3076438b212a8a1de60b6233d1e576d3
harishgowda121 <161109259+harishgowda121@users.noreply.github.com>
2025-09-22 18:55:33 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    mobile: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    email: { type: String },
-    role: { type: String, default: 'user' },   // Role added here
-    otp: { type: String },
-    otpExpiry: { type: Date }
-});
+  mobile: { type: String, required: true, unique: true },
+  role: { type: String, default: 'user' },
+  otp: { type: String },
+  otpExpiry: { type: Date }
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
