376b23b19fa6a7eaf450936dd06e2556f239771d
harishgowda121 <161109259+harishgowda121@users.noreply.github.com>
2025-09-23 08:55:49 +0530

    Update Hotel.js

--- models/Hotel.js
+++ models/Hotel.js
@@ -9,11 +9,25 @@ const MenuSchema = new mongoose.Schema({
 
 const HotelSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    image_url: { type: String },
     description: { type: String },
     stations: { type: [String], required: true },
     owner_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+
+    // Documents & Images
+    logo: { type: String },
+    background_image: { type: String },
+    gst_license: { type: String },
+    fssai_license: { type: String },
+
+    // Verification
+    verified: { type: Boolean, default: false },
+
+    // Kitchen
+    isOpen: { type: Boolean, default: false },
+
+    // Menu
     menu: [MenuSchema],
+
     created_at: { type: Date, default: Date.now }
 });
 
